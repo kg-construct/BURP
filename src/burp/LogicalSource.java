9ee72226fb2833f7cee3588741644c37c601a762
chrdebru <christophe.debruyne@gmail.com>
2024-02-15 19:25:59 +0100

    Fixed small bug in logical source

--- src/burp/LogicalSource.java
+++ src/burp/LogicalSource.java
@@ -9,26 +9,30 @@ import com.opencsv.CSVReader;
 
 public abstract class LogicalSource {
 
-	public List<Iteration> iterations = new ArrayList<Iteration>();
 	protected abstract Iterator<Iteration> iterator();
 
 }
 
 class CSVSource extends LogicalSource {
 	
+	private List<Iteration> iterations = null;
 	public String file;
 
 	@Override
 	protected Iterator<Iteration> iterator() {
 		try {
-			FileReader fr = new FileReader(file); 
-			CSVReader reader = new CSVReader(fr); 
-			List<String[]> all = reader.readAll();
-			reader.close();
-			
-			String[] header = all.remove(0);
-			for(String[] rec : all) {
-				iterations.add(new CSVIteration(header, rec));
+			if(iterations == null) {
+				iterations = new ArrayList<Iteration>();
+				
+				FileReader fr = new FileReader(file); 
+				CSVReader reader = new CSVReader(fr); 
+				List<String[]> all = reader.readAll();
+				reader.close();
+				
+				String[] header = all.remove(0);
+				for(String[] rec : all) {
+					iterations.add(new CSVIteration(header, rec));
+				}
 			}
 			return iterations.iterator();
 		} catch (Throwable e) {
