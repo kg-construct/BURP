9870eb9907370d7247f01f9bbd93ffc03e88a1e0
Christophe Debruyne <christophe.debruyne@gmail.com>
2024-04-08 19:54:59 +0200

    Changed R2RML into RML (copy/paste from R2RML-F)

--- src/main/java/burp/util/BURPConfiguration.java
+++ src/main/java/burp/util/BURPConfiguration.java
@@ -4,19 +4,19 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "urml")
+@Command(name = "burp")
 public class BURPConfiguration {
 
 	@Option(names = {"-h", "--help" }, usageHelp = true, description = "Display a help message")
 	public boolean help = false;
 
-	@Option(names= {"-m", "--mappingFile"}, description = "The R2RML mapping file", required = true)
+	@Option(names= {"-m", "--mappingFile"}, description = "The RML mapping file", required = true)
 	public String mappingFile = null;
 
 	@Option(names= {"-o", "--outputFile"}, description = "The output file", required = false)
 	public String outputFile = null;
 
-	@Option(names = {"-b", "--baseIRI"}, description = "Used in resolving relative IRIs produced by the R2RML mapping" )
+	@Option(names = {"-b", "--baseIRI"}, description = "Used in resolving relative IRIs produced by the RML mapping" )
 	public String baseIRI = null;
 	
 	public BURPConfiguration(String[] args) throws Exception {
