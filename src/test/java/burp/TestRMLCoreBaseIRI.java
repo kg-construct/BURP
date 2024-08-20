c1ec3a5893935ce522035b20930e054d0209ada2
Christophe Debruyne <christophe.debruyne@gmail.com>
2024-08-20 17:44:58 +0200

    Added some tests based on rml-core without rml:baseIRI directives

---
+++ src/test/java/burp/TestRMLCoreBaseIRI.java
@@ -0,0 +1,68 @@
+package burp;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestRMLCoreBaseIRI {
+	private static String base = "./src/test/resources/rml-core-baseiri/";
+
+	@Test public void RMLTC0001() throws IOException { testForOK("RMLTC0024a"); }
+	@Test public void RMLTC0002() throws IOException { testForOK("RMLTC0024b"); }
+
+	@Test public void RMLTC0003() throws IOException { testForNotOK("RMLTC0024c"); }
+	
+	public void testForOK(String f) throws IOException {
+		System.out.println(String.format("Now processing %s", f));
+		String m = new File(base + f, "mapping.ttl").getAbsolutePath().toString();
+		String r = Files.createTempFile(null, ".nq").toString();
+		System.out.println(String.format("Writing output to %s", r));
+
+		System.out.println("This test should generate a graph.");
+		String o = new File(base + f, "output.nq").getAbsolutePath().toString();
+
+		int exit = Main.doMain(new String[] { "-m", m, "-o", r, "-b", "http://example.com/base/" });
+
+		Model expected = RDFDataMgr.loadModel(o);
+		Model actual = RDFDataMgr.loadModel(r);
+
+		if (!expected.isIsomorphicWith(actual)) {
+			expected.write(System.out, "NQ");
+			System.out.println();
+			actual.write(System.out, "NQ");
+		}
+
+		assertEquals(0, exit);
+
+		System.out.println(expected.isIsomorphicWith(actual) ? "OK" : "NOK");
+
+		assertTrue(expected.isIsomorphicWith(actual));
+	}
+
+	public void testForNotOK(String f) throws IOException {
+		System.out.println(String.format("Now processing %s", f));
+		String m = new File(base + f, "mapping.ttl").getAbsolutePath().toString();
+		String r = Files.createTempFile(null, ".nq").toString();
+		System.out.println(String.format("Writing output to %s", r));
+
+		System.out.println("This test should NOT generate a graph.");
+		int exit = Main.doMain(new String[] { "-m", m, "-o", r });
+		System.out.println(Files.size(Paths.get(r)) == 0 ? "OK" : "NOK");
+		Model actual = RDFDataMgr.loadModel(r);
+		actual.write(System.out, "NQ");
+
+		assertTrue(exit > 0);
+		assertTrue(Files.size(Paths.get(r)) == 0);
+
+		System.out.println();
+	}
+
+}
---
+++ src/test/resources/rml-core-baseiri/README.md
@@ -0,0 +1,5 @@
+| Test case  | Fail? | Description                                                                      |
+|------------|-------|----------------------------------------------------------------------------------|
+| RMLTC0024a | No    | Absolute IRIs in mappings different from baseIRI provided as (commandline) input |
+| RMLTC0024b | No    | Relative IRIs in mappings and baseIRI provided as (commandline) input            |
+| RMLTC0024c | Yes   | Relative IRIs in mappings and **no** baseIRI provided as (commandline) input     |
