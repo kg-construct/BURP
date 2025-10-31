b0049b2e27f39739e73738ccd3db05038cc64bfe
Christophe Debruyne <christophe.debruyne@gmail.com>
2025-10-31 16:32:26 +0100

    All LV tests pass.

--- src/main/java/burp/model/ViewJoin.java
+++ src/main/java/burp/model/ViewJoin.java
@@ -30,15 +30,22 @@ public class ViewJoin {
             for (LogicalIteration childIteration : childIterations) {
                 boolean hasACorrespondence = false;
 
+                // Discussion with Els. For each child, the counter for the parents are "reset" to 0
+                // So we assign 0, 1, ..., n from the child's perspective.
+                int count = -1; // start at -1 as no match found yet
+
                 for(LogicalIteration parentIteration : iterations) {
                     if(matches(childIteration, parentIteration)) {
                         hasACorrespondence = true;
+
+                        // Increment the index
+                        count++;
 
                         // Create new logical iterations for each value of each expression map
                         List<LogicalIteration> result = new ArrayList<>();
                         result.add(childIteration);
                         for (ExpressionField e : expressionFields) {
-                            result = enrichForJoin(e, result, parentIteration);
+                            result = enrichForJoin(e, count, result, parentIteration);
                         }
 
                         // add all new iterations to the list
@@ -63,15 +70,14 @@ public class ViewJoin {
         }
     }
 
-    private List<LogicalIteration> enrichForJoin(ExpressionField e, List<LogicalIteration> result, LogicalIteration parentIteration) {
+    private List<LogicalIteration> enrichForJoin(ExpressionField e, int index, List<LogicalIteration> result, LogicalIteration parentIteration) {
         List<LogicalIteration> nlist = new ArrayList<>();
 
         for(LogicalIteration li : result) {
-            int count = 0;
             for (Object o : e.fieldExpressionMap.generateValues(parentIteration)) {
                 LogicalIteration newLogicalIteration = li.copy();
                 newLogicalIteration.put(e.fieldName, o);
-                newLogicalIteration.put(e.fieldName + ".#", count++);
+                newLogicalIteration.put(e.fieldName + ".#", index);
                 nlist.add(newLogicalIteration);
             }
         }
