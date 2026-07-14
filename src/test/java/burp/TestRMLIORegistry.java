5c8544471ec172d1f614064c42201a51b30af210
Jakub Duchateau <jakub.duchateau@uliege.be>
2026-07-14 15:19:45 +0200

    test: use Testcontainers annotations for managing the lifecycle of database containers

--- src/test/java/burp/TestRMLIORegistry.java
+++ src/test/java/burp/TestRMLIORegistry.java
@@ -6,9 +6,9 @@ import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.update.UpdateAction;
 import org.apache.jena.update.UpdateFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mssqlserver.MSSQLServerContainer;
 import org.testcontainers.mysql.MySQLContainer;
 import org.testcontainers.postgresql.PostgreSQLContainer;
@@ -18,10 +18,8 @@ import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Stream;
 
-
+@Testcontainers
 public class TestRMLIORegistry extends TestRMLModule {
 
     public static String base = "./target/test-classes/rml-io-registry/";
@@ -46,19 +44,16 @@ public class TestRMLIORegistry extends TestRMLModule {
 
             switch (jdbcDriver) {
                 case "com.mysql.cj.jdbc.Driver":
-                    MYSQL_CONTAINER_FUTURE.join();
                     queryString = "?allowMultiQueries=true";
                     MYSQL_CONTAINER.addParameter("allowMultiQueries", "true");
                     db = MYSQL_CONTAINER;
                     break;
 
                 case "org.postgresql.Driver":
-                    PGSQL_CONTAINER_FUTURE.join();
                     db = PGSQL_CONTAINER;
                     break;
 
                 case "com.microsoft.sqlserver.jdbc.SQLServerDriver":
-                    MSSQL_CONTAINER_FUTURE.join();
                     MSSQL_CONTAINER.withUrlParam("databaseName", "master");
                     try (var conn = MSSQL_CONTAINER.createConnection("")) {
                         try (var stmt = conn.createStatement()) {
@@ -78,10 +73,10 @@ public class TestRMLIORegistry extends TestRMLModule {
             System.out.printf("Updating database connection in mapping with JDBC URL=%s Username=%s Password=%s%n", db.getJdbcUrl(), db.getUsername(), db.getPassword());
             String sparql = String.format(
                     """
-                    PREFIX d2rq: <http://www.wiwiss.fu-berlin.de/suhl/bizer/D2RQ/0.1#>
-                    DELETE { ?s d2rq:jdbcDSN ?oldDSN; d2rq:username ?oldUser; d2rq:password ?oldPass }
-                    INSERT { ?s d2rq:jdbcDSN "%s"; d2rq:username "%s"; d2rq:password "%s" }
-                    WHERE { ?s d2rq:jdbcDSN ?oldDSN; d2rq:username ?oldUser; d2rq:password ?oldPass }""",
+                            PREFIX d2rq: <http://www.wiwiss.fu-berlin.de/suhl/bizer/D2RQ/0.1#>
+                            DELETE { ?s d2rq:jdbcDSN ?oldDSN; d2rq:username ?oldUser; d2rq:password ?oldPass }
+                            INSERT { ?s d2rq:jdbcDSN "%s"; d2rq:username "%s"; d2rq:password "%s" }
+                            WHERE { ?s d2rq:jdbcDSN ?oldDSN; d2rq:username ?oldUser; d2rq:password ?oldPass }""",
                     db.getJdbcUrl(), db.getUsername(), db.getPassword()
             );
             var updateRequest = UpdateFactory.create(sparql);
@@ -111,31 +106,23 @@ public class TestRMLIORegistry extends TestRMLModule {
         }
     }
 
+    @Container
     public static PostgreSQLContainer PGSQL_CONTAINER = new PostgreSQLContainer("postgres:latest")
             .withUsername("postgres")
-            .withPassword("test");
-    private static CompletableFuture<Void> PGSQL_CONTAINER_FUTURE = null;
+            .withPassword("test")
+            .withReuse(true);
 
+    @Container
     public static MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:8")
             .withEnv("MYSQL_ROOT_HOST", "%")
-            .withCommand("mysqld", "--sql_mode=ANSI_QUOTES");
-    private static CompletableFuture<Void> MYSQL_CONTAINER_FUTURE = null;
+            .withCommand("mysqld", "--sql_mode=ANSI_QUOTES")
+            .withReuse(true);
 
-    public static MSSQLServerContainer MSSQL_CONTAINER = new MSSQLServerContainer("mcr.microsoft.com/mssql/server:2022-CU20-ubuntu-22.04")
-            .acceptLicense();
-    private static CompletableFuture<Void> MSSQL_CONTAINER_FUTURE = null;
 
-    @BeforeAll
-    public static void startContainers() {
-        PGSQL_CONTAINER_FUTURE = CompletableFuture.runAsync(PGSQL_CONTAINER::start);
-        MYSQL_CONTAINER_FUTURE = CompletableFuture.runAsync(MYSQL_CONTAINER::start);
-        MSSQL_CONTAINER_FUTURE = CompletableFuture.runAsync(MSSQL_CONTAINER::start);
-    }
+    @Container
+    public static MSSQLServerContainer MSSQL_CONTAINER =
+            new MSSQLServerContainer("mcr.microsoft.com/mssql/server:2022-CU20-ubuntu-22.04")
+                    .acceptLicense()
+                    .withReuse(true);
 
-    @AfterAll
-    public static void stopContainers() {
-        Stream.of(PGSQL_CONTAINER, MYSQL_CONTAINER, MSSQL_CONTAINER)
-                .parallel()
-                .forEach(JdbcDatabaseContainer::stop);
-    }
 }
